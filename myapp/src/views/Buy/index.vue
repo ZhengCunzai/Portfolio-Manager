8c5d77f58f610b9124f3fe76aa5cd9b78bbce133
ZhengCunzai <1239431331@qq.com>
2021-08-24 16:16:21 +0800

    init: first commit

---
+++ myapp/src/views/Buy/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-container>
+    <my-layout>
+      <el-input placeholder="Please Input FundID" v-model="input" clearable>
+      </el-input>
+      <el-button type="primary" icon="el-icon-s-goods" @click="BuySubmit"
+        >Buy This Fund</el-button>
+    </my-layout>
+  </el-container>
+</template>
+<script>
+import Layout from "../../components/Layout.vue";
+export default {
+  components: {
+    "my-layout": Layout,
+  },
+  data() {
+    return {
+      input: "",
+    };
+  },
+  methods: {
+    BuySubmit() {
+     this.$confirm("Are you sure to Buy this Fund?", "Verify", {
+       distinguishCancelAndClose: true,
+        confirmButtonText: "BUY!",
+        cancelButtonText: "Cancle",
+        type: "warning",
+      })
+        .then(() => {
+          this.$message({
+            type: "success",
+            message: "Success!",
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "Cancled",
+          });
+        });
+    },
+  },
+};
+</script>
---
+++ myapp/src/views/Login/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <el-input placeholder="Please Input Your Email" v-model="input" clearable>
+    </el-input>
+    <el-input
+      placeholder="Please Input Your Password"
+      v-model="input"
+      show-password
+    ></el-input>
+    <el-button @click="LOGIN"> Login </el-button>
+
+    <el-button @click="dialogFormVisible = true">New User</el-button>
+    <el-dialog title="New User Information" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="First Name" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">
+          确 定
+        </el-button>
+      </div>
+    </el-dialog>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        date1: "",
+        date2: "",
+        delivery: false,
+        type: [],
+        resource: "",
+        desc: "",
+      },
+      formLabelWidth: "120px",
+      input: "",
+    };
+  },
+  methods: {
+    LOGIN() {
+      this.$router.push({
+        path: "/showOneFund",
+      });
+    },
+  },
+};
+</script>
