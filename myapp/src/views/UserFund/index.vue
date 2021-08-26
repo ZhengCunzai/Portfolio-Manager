921071f59ed2e9b2cdd298e92362232d19d4633d
ZhengCunzai <1239431331@qq.com>
2021-08-26 16:25:08 +0800

    second: commint

--- myapp/src/views/UserFund/index.vue
+++ myapp/src/views/UserFund/index.vue
@@ -1,13 +1,25 @@
 <template>
-<el-container>
-    <my-layout></my-layout>
-</el-container>
+  <my-layout>
+   <fund-table></fund-table>
+    <el-backtop target=".page-component__scroll .el-scrollbar__wrap"></el-backtop>
+  </my-layout>
 </template>
+
+
 <script>
 import Layout from "../../components/Layout.vue";
+import MyFundTable from "./components/MyFundTable.vue";
+
 export default {
   components: {
     "my-layout": Layout,
-  },
+    "fund-table": MyFundTable,
+  },      
 };
 </script>
+
+<style scoped>
+.el-table {
+  width: 100%;
+}
+</style>
