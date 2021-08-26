6c8390d90ad4f71e957cfde6dc2b86e091be2612
ZhengCunzai <1239431331@qq.com>
2021-08-27 00:11:38 +0800

    add query

---
+++ myapp/src/views/Query/components/Account.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <el-date-picker
+      v-model="value1"
+      type="daterange"
+      range-separator="至"
+      start-placeholder="开始日期"
+      end-placeholder="结束日期"
+      value-format="yyyy-MM-dd"
+    >
+    </el-date-picker>
+    <el-button @click="handleQuery">查询</el-button>
+    <el-table
+      :data="
+        tableData.filter(
+          (data) =>
+            !search || data.symble.toLowerCase().includes(search.toLowerCase())
+        )
+      "
+      style="width: 100%"
+    >
+      <el-table-column label="Income" prop="income"> </el-table-column>
+      <el-table-column label="Spend" prop="spend"> </el-table-column>
+      <el-table-column label="Balance" prop="balance"> </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+
+<script>
+import { getAccount } from "../../../api/querry";
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          income: 1482,
+          spend: "-12000",
+          balance: 12000,
+        },
+      ],
+      value1: "",
+    };
+  },
+  components: {},
+  methods: {
+    handleQuery() {
+      console.log(this.value1[0], this.value1[1]);
+      getAccount(this.value1[0], this.value1[1]).then(
+        (res) => {
+          if (res.code === 200) {
+            this.tableData = res.data;
+            this.$message.success({ duration: 500, message: "查询成功" });
+          } else {
+            this.$message.error({ duration: 500, message: res.message });
+          }
+        },
+        (res) => {
+          this.$message.error({ duration: 500, message: res.message });
+        }
+      );
+    },
+  },
+};
+</script>
+
+<style scoped>
+.el-table {
+  width: 100%;
+}
+</style>
---
+++ myapp/src/views/Query/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <my-layout>
+    <el-tabs v-model="activeName" >
+      <el-tab-pane label="transaction" name="transaction"
+        ><my-transaction
+      /></el-tab-pane>
+      <el-tab-pane label="trade" name="trade"><my-trade /></el-tab-pane>
+      <el-tab-pane label="account" name="account"><my-account /></el-tab-pane>
+    </el-tabs>
+  </my-layout>
+</template>
+<script>
+import Transaction from "./components/Transaction";
+import Trade from "./components/Trade";
+import Account from "./components/Account.vue";
+import Layout from "../../components/Layout.vue";
+export default {
+  components: {
+    "my-transaction": Transaction,
+    "my-trade": Trade,
+    "my-account": Account,
+    "my-layout": Layout,
+  },
+  data() {
+    return {
+      activeName: "transaction",
+    };
+  },
+};
+</script>
