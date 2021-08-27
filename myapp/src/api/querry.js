3f929eb7ea8ef8a1daae14f4d48a877e85bbe22a
ZhengCunzai <1239431331@qq.com>
2021-08-27 10:03:09 +0800

    qurry baseurl

--- myapp/src/api/querry.js
+++ myapp/src/api/querry.js
@@ -1,20 +1,20 @@
 import axios from "axios";
-const baseUrl = '127.0.0.1:8000'
+const baseUrl = '127.0.0.1:8090'
 
-export function getTransaction() {
+export function getTransaction(beginDate, endDate) {
     return axios({
         url: baseUrl + `account/showTransaction/${beginDate}/${endDate}`,
         method: 'get',
     })
-}
-export function getTrade() {
+ }
+export function getTrade(beginDate, endDate) {
     return axios({
         url: baseUrl + `account/showTrade/${beginDate}/${endDate}`,
         method: 'get',
     })
 }
 
-export function getAccount() {
+export function getAccount(beginDate, endDate) {
     return axios({
         url: baseUrl + `account/showAccount/${beginDate}/${endDate}`,
         method: 'get',
