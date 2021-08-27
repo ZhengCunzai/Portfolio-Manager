b75eff625e75bf9d9f5b28e56456c775a7eb9c8d
Baron <barondu1996@gmail.com>
2021-08-27 14:04:31 +0800

    final

--- myapp/src/views/Dashboard/index.vue
+++ myapp/src/views/Dashboard/index.vue
@@ -2,9 +2,9 @@
   <my-layout>
     <h1>Market Details (IBM)</h1>
     <my-linechart></my-linechart>
-    <h1> Portfolio (My investment)</h1>
+    <h1> Portfolio (investment account)</h1>
     <my-piechart></my-piechart>
-    <h1> Profit ((My investment))</h1>
+    <h1> Profit ((investment account))</h1>
     <my-profitchart></my-profitchart>
 <!--    <el-button type="primary" @click="Start">Start</el-button>-->
   </my-layout>
